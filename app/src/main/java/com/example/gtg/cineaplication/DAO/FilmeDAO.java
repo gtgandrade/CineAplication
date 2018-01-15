a7fe2eec8fec94d666e0437fbb1e80289922fce8
Mário Santos <marioh90@gmail.com>
2018-01-15 00:41:08 -0300

    Reestruturação do projeto.
    Criação do CRUD para Horários, a classe HorarioDAO foi criada para isso.
    Excluída classe HorarioBD, pois era uma estrutura desnecessária.

--- app/src/main/java/com/example/gtg/cineaplication/DAO/FilmeDAO.java
+++ app/src/main/java/com/example/gtg/cineaplication/DAO/FilmeDAO.java
@@ -3,92 +3,89 @@ package com.example.gtg.cineaplication.DAO;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-
 import com.example.gtg.cineaplication.conexao.Conexao;
 import com.example.gtg.cineaplication.modelo.Filme;
-
 import java.util.ArrayList;
 import java.util.List;
 
+public class FilmeDAO {
+	private Conexao conexao;
+	private Cursor cursor;
 
-public class FilmeDAO{
-    private Conexao conexao;
-    private Cursor cursor;
+	public FilmeDAO(Context context) {
+		this.conexao = Conexao.getInstance(context);
+	}
 
-    public FilmeDAO(Context context) {
-        this.conexao = Conexao.getInstance(context);
-    }
+	public boolean atualizar(Filme filme) {
+		long salvo;
 
-    public boolean salvar(Filme filme){
-        long salvo;
+		ContentValues valoresCampos = new ContentValues();
+		valoresCampos.put("nome", filme.getCodigo());
+		valoresCampos.put("codigo", filme.getCodigo());
+		valoresCampos.put("nome", filme.getNome());
+		valoresCampos.put("pais", filme.getPais());
+		valoresCampos.put("versao", filme.getVersao());
+		valoresCampos.put("duracao", filme.getDuracao());
+		valoresCampos.put("habilitado", filme.getHabilitado());
 
-        ContentValues valoresCampos = new ContentValues();
-        valoresCampos.put("nome", filme.getCodigo());
-        valoresCampos.put("codigo", filme.getCodigo());
-        valoresCampos.put("nome", filme.getNome());
-        valoresCampos.put("pais", filme.getPais());
-        valoresCampos.put("versao", filme.getVersao());
-        valoresCampos.put("duracao", filme.getDuracao());
-        valoresCampos.put("habilitado", filme.getHabilitado());
-        salvo = conexao.getDatabase().insert("filme",null, valoresCampos);
+		String condicaoWhere = "idfilme = '" + filme.getIdfilme() + "'";
+		salvo = conexao.getWritable().update("filme", valoresCampos, condicaoWhere, null);
 
-        return salvo > 0? true : false;
-    }
+		return (salvo > 0);
+	}
 
-    public boolean atualizar(Filme filme){
-        long salvo;
+	public boolean excluir(Filme filme) {
+		int excluiu;
+		String condicaoWhere = "idfilme = '" + filme.getIdfilme() + "'";
+		excluiu = conexao.getWritable().delete("filme", condicaoWhere, null);
 
-        ContentValues valoresCampos = new ContentValues();
-        valoresCampos.put("nome", filme.getCodigo());
-        valoresCampos.put("codigo", filme.getCodigo());
-        valoresCampos.put("nome", filme.getNome());
-        valoresCampos.put("pais", filme.getPais());
-        valoresCampos.put("versao", filme.getVersao());
-        valoresCampos.put("duracao", filme.getDuracao());
-        valoresCampos.put("habilitado", filme.getHabilitado());
+		return excluiu > 0;
+	}
 
-        String condicaoWhere = "idfilme = '"+filme.getIdfilme()+"'";
-        salvo = conexao.getDatabase().update("filme", valoresCampos, condicaoWhere, null);
+	public List<Filme> procurarTodos() {
+		List<Filme> filmes = new ArrayList<>();
+		cursor = conexao.getWritable().query(false, "filme", null, null, null,
+			null, null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				Filme filme = new Filme();
+				filme.setIdfilme(cursor.getInt(0));
+				filme.setCodigo(cursor.getInt(1));
+				filme.setNome(cursor.getString(2));
+				filmes.add(filme);
+			} while (cursor.moveToNext());
+		}
 
-        return salvo > 0? true : false;
-    }
+		return filmes;
+	}
 
-    public boolean excluir(Filme filme){
-        int excluiu = 0;
-        String condicaoWhere = "idfilme = '"+filme.getIdfilme()+"'";
-        excluiu = conexao.getDatabase().delete("filme", condicaoWhere, null);
+	public boolean salvar(Filme filme) {
+		long salvo;
 
-        return excluiu > 0? true: false;
-    }
+		ContentValues valoresCampos = new ContentValues();
+		valoresCampos.put("nome", filme.getCodigo());
+		valoresCampos.put("codigo", filme.getCodigo());
+		valoresCampos.put("nome", filme.getNome());
+		valoresCampos.put("pais", filme.getPais());
+		valoresCampos.put("versao", filme.getVersao());
+		valoresCampos.put("duracao", filme.getDuracao());
+		valoresCampos.put("habilitado", filme.getHabilitado());
+		salvo = conexao.getWritable().insert("filme", null, valoresCampos);
 
-    public Filme procurarPorId(int idfilme){
-        Filme filme = new Filme();
-        String condicaoWhere = "idfilme = '"+idfilme+"'";
-        cursor = conexao.getDatabase().query("filme",null, condicaoWhere,null,
-                    null, null, null, null);
-        if(cursor.moveToFirst()){
-            filme.setIdfilme(cursor.getInt(0));
-            filme.setCodigo(cursor.getInt(1));
-            filme.setNome(cursor.getString(2));
-        }
+		return salvo > 0;
+	}
 
-        return  filme;
-    }
+	Filme procurarPorId(int idfilme) {
+		Filme filme = new Filme();
+		String condicaoWhere = "idfilme = '" + idfilme + "'";
+		cursor = conexao.getReadable().query("filme", null, condicaoWhere, null,
+			null, null, null, null);
+		if (cursor.moveToFirst()) {
+			filme.setIdfilme(cursor.getInt(0));
+			filme.setCodigo(cursor.getInt(1));
+			filme.setNome(cursor.getString(2));
+		}
 
-    public List<Filme> procurarTodos(){
-        List<Filme> filmes = new ArrayList<Filme>();
-        cursor = conexao.getDatabase().query(false,"filme",null,null,null,
-                    null, null, null, null);
-        if(cursor.moveToFirst()){
-            do{
-                Filme filme = new Filme();
-                filme.setIdfilme(cursor.getInt(0));
-                filme.setCodigo(cursor.getInt(1));
-                filme.setNome(cursor.getString(2));
-                filmes.add(filme);
-            }while(cursor.moveToNext());
-        }
-
-        return  filmes;
-    }
+		return filme;
+	}
 }
--- app/src/main/java/com/example/gtg/cineaplication/conexao/BancoDados.java
+++ app/src/main/java/com/example/gtg/cineaplication/conexao/BancoDados.java
@@ -4,59 +4,61 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by gtg on 31/12/17.
- */
-
+/** Created by gtg on 31/12/17. */
 public class BancoDados extends SQLiteOpenHelper {
-    private static final String DB_NAME = "bdcinema.db";
-    private static final int DB_VERSION = 1;
-    private String comandosql;
+	private static final String DB_NAME = "bdcinema.db";
+	private static final int DB_VERSION = 1;
+	private String comandosql;
 
-    public BancoDados(Context context){
-        super(context, DB_NAME, null, DB_VERSION);
-    }
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        comandosql = "CREATE TABLE filme (idfilme    INTEGER      PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                                         "codigo     INTEGER," +
-                                         "nome       VARCHAR (50),"+
-                                         "pais       VARCHAR (30),"+
-                                         "versao     VARCHAR (20),"+
-                                         "duracao    INTEGER,"+
-                                         "habilitado INTEGER)";
-        db.execSQL(comandosql);
+	public static final String TB_HORARIO = "horario";
+	public static final String HR_ID = "idhorario";
+	public static final String HR_DESCRICAO = "descricao";
 
-        comandosql = "CREATE TABLE horario (idhorario INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                                           "descricao VARCHAR (5))";
-        db.execSQL(comandosql);
+	BancoDados(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+	}
 
-        comandosql = "CREATE TABLE sessao (idsessao INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                                          "sala INTEGER, " +
-                                          "filme_idfilme INTEGER REFERENCES filme (idfilme) NOT NULL, " +
-                                          "horario_idhorario INTEGER REFERENCES horario (idhorario) NOT NULL)";
-        db.execSQL(comandosql);
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		comandosql = "CREATE TABLE filme (idfilme    INTEGER      PRIMARY KEY AUTOINCREMENT NOT NULL," +
+			"codigo     INTEGER," +
+			"nome       VARCHAR (50)," +
+			"pais       VARCHAR (30)," +
+			"versao     VARCHAR (20)," +
+			"duracao    INTEGER," +
+			"habilitado INTEGER)";
+		db.execSQL(comandosql);
 
-        comandosql = "CREATE TABLE ingresso (idingresso INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                                            "precounitario DOUBLE, " +
-                                            "qtdinteira INTEGER, " +
-                                            "qtdmeia INTEGER, " +
-                                            "qtdlanche INTEGER, " +
-                                            "precolanche DOUBLE, " +
-                                            "sessao_idsesso INTEGER REFERENCES sessao (idsessao) NOT NULL)";
-        db.execSQL(comandosql);
-    }
+		comandosql = "CREATE TABLE horario (idhorario INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+			"descricao VARCHAR (5))";
+		db.execSQL(comandosql);
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        comandosql = "DROP TABLE IF EXISTS ingresso";
-        db.execSQL(comandosql);
-        comandosql = "DROP TABLE IF EXISTS sessao";
-        db.execSQL(comandosql);
-        comandosql = "DROP TABLE IF EXISTS filme";
-        db.execSQL(comandosql);
-        comandosql = "DROP TABLE IF EXISTS horario";
-        db.execSQL(comandosql);
-        onCreate(db);
-    }
+		comandosql = "CREATE TABLE sessao (idsessao INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+			"sala INTEGER, " +
+			"filme_idfilme INTEGER REFERENCES filme (idfilme) NOT NULL, " +
+			"horario_idhorario INTEGER REFERENCES horario (idhorario) NOT NULL)";
+		db.execSQL(comandosql);
+
+		comandosql = "CREATE TABLE ingresso (idingresso INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+			"precounitario DOUBLE, " +
+			"qtdinteira INTEGER, " +
+			"qtdmeia INTEGER, " +
+			"qtdlanche INTEGER, " +
+			"precolanche DOUBLE, " +
+			"sessao_idsesso INTEGER REFERENCES sessao (idsessao) NOT NULL)";
+		db.execSQL(comandosql);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+		comandosql = "DROP TABLE IF EXISTS ingresso";
+		db.execSQL(comandosql);
+		comandosql = "DROP TABLE IF EXISTS sessao";
+		db.execSQL(comandosql);
+		comandosql = "DROP TABLE IF EXISTS filme";
+		db.execSQL(comandosql);
+		comandosql = "DROP TABLE IF EXISTS horario";
+		db.execSQL(comandosql);
+		onCreate(db);
+	}
 }
